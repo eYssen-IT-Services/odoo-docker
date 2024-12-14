acd5119fd7133742a30358b2231818c9e604c2ec
Krisztian Eyssen <krisz@eyssen.hu>
2024-12-14 17:26:42 +0100

    .

---
+++ 18.0-coder/get_db_name.py
@@ -0,0 +1,10 @@
+import configparser
+import sys
+
+def get_db_name(config_file="/etc/odoo/odoo.conf"):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    return config.get("options", "db_name", fallback="default")
+
+if __name__ == "__main__":
+    print(get_db_name())
